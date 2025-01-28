9f3d3a611fb3c2d927804cd68a509469310df69c
AdroitPhilocalist <sahapiyush5@gmail.com>
2025-01-28 11:01:45 +0530

    5TH PROG

--- Day02/d2p5.sol
+++ Day02/d2p5.sol
@@ -1,27 +1,26 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
 
 contract SwapVariables {
     
     uint256 public a;
     uint256 public b;
 
-    constructor(uint256 _a, uint256 _b) {
+    function setInput(uint256 _a, uint256 _b)public {
         a = _a;
         b = _b;
     }
 
-    // Swap using a third variable
     function swapWithThirdVariable() public {
-        uint256 temp = a;  // Store value of a in temp
-        a = b;             // Assign value of b to a
-        b = temp;          // Assign value of temp (original a) to b
+        uint256 temp = a;  
+        a = b;             
+        b = temp;          
     }
 
     // Swap without using a third variable
     function swapWithoutThirdVariable() public {
-        a = a + b;         // Step 1: a = a + b
-        b = a - b;         // Step 2: b = a - b (now b becomes the original a)
-        a = a - b;         // Step 3: a = a - b (now a becomes the original b)
+        a = a + b;         
+        b = a - b;         
+        a = a - b;         
     }
 }
